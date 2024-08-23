6eb4ea24306d1060fc51b481bd86928a3d00c6ed
RomanBabych <rrroma.babi42017@gmail.com>
2024-08-23 20:32:46 +0300

    homework done

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, '[]');
+    console.log('All contacts removed successfully.');
+  } catch (error) {
+    console.error('Error removing all contacts:', error);
+  }
+};
 
 removeAllContacts();
